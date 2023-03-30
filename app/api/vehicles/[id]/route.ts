d148f39564af96be5ffa2acd56acdbf97d92d82e
Nabster2010 <58792560+Nabster2010@users.noreply.github.com>
2023-03-31 02:24:24 +0300

    fix typescript

--- app/api/vehicles/[id]/route.ts
+++ app/api/vehicles/[id]/route.ts
@@ -5,16 +5,25 @@ import {
 } from "@/lib/prisma/vehicle";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }) {
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   const vehicle = await getVehicleById(parseInt(params.id));
   return NextResponse.json({ vehicle });
 }
-export async function PUT(req: Request, { params }) {
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   const data = await req.json();
   const vehicle = await updateVehicle(data, parseInt(params.id));
   return NextResponse.json({ vehicle });
 }
-export async function DELETE(req: Request, { params }) {
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   const vehicle = await deleteVehicle(parseInt(params.id));
   return NextResponse.json({ vehicle });
 }
